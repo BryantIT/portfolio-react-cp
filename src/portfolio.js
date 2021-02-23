a2961063a8bd1eaf220626f3a3a9e25a5c588648
Bryant Richards <dragonseyeimages@gmail.com>
2021-02-23 06:41:33 -0600

    updated education

--- src/portfolio.js
+++ src/portfolio.js
@@ -99,26 +99,13 @@ https://fontawesome.com/icons?d=gallery */
 // Education Section
 
 const educationInfo = {
-  display: false, // Set false to hide this section, defaults to true
+  display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
-      descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-      ]
-    },
-    {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc:
-        "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+      schoolName: "Flatiron School",
+      logo: require("./assets/images/FS_wiki.png"),
+      subHeader: "Software Engineering Intensive",
+      duration: "September 2019 - May 2020",
     }
   ]
 };
